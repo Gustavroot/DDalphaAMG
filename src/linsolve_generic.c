78ac9afc07d59538874d29f9db0125e5e79b9c05
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2022-11-16 11:32:50 +0100

    reenable xtmp initialization to NULL
    this fixes an abort error caused by a bad free

--- src/linsolve_generic.c
+++ src/linsolve_generic.c
@@ -33,7 +33,7 @@ void fgmres_PRECISION_struct_init( gmres_PRECISION_struct *p ) {
   p->H = NULL;
   p->x = NULL;
 #ifdef CUDA_OPT
-  //p->xtmp = NULL;
+  p->xtmp = NULL;
 #endif
   p->b = NULL;
   p->r = NULL;
@@ -204,7 +204,7 @@ void fgmres_PRECISION_struct_free( gmres_PRECISION_struct *p, level_struct *l ) 
 *********************************************************************************/ 
 
 #ifdef CUDA_OPT
-  if( l->depth==0 ) cuda_safe_call( cudaFreeHost( l->p_PRECISION.xtmp ) );
+  if( l->depth==0) cuda_safe_call( cudaFreeHost( l->p_PRECISION.xtmp ) );
 #endif
 
 #ifdef CUDA_OPT
