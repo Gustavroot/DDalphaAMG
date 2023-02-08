1f7d81197fa398983985b9069953d945c37a548a
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2023-02-08 13:45:32 +0100

    some documentation work

--- src/gpu/cuda_dirac_kernels_generic.h
+++ src/gpu/cuda_dirac_kernels_generic.h
@@ -1,6 +1,23 @@
+/**
+ * \file cuda_dirac_kernels_generic.h
+ * 
+ * \brief CUDA kernels to perform calculations related to application of the Wilson-Dirac operator.
+ */
+
 #ifndef CUDA_DIRAC_KERNELS_PRECISION_H
 #define CUDA_DIRAC_KERNELS_PRECISION_H
 
+/**
+ * \brief Apply the Clover term per lattice site.
+ * 
+ * \param[out]  eta         Set to eta = C * phi.
+ *                          I.e. is the result of applying the Clover term on phi.
+ * \param[in]   phi         Quark field phi.
+ * \param[in]   clover      The configuration of the Clover term.
+ * \param[in]   num_sites   The number of lattice sites to process. A call which starts more threads
+ *                          than needed for the number of lattice sites will have the remaining
+ *                          threads idle/return.
+ */
 __global__ void cuda_site_clover_PRECISION(cuda_vector_PRECISION eta, cuda_vector_PRECISION phi,
                                            cuda_config_PRECISION clover, size_t num_sites);
 
