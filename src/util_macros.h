4b3e82fe55ad0435f90f4f9c665e404a4cc9b5ec
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2023-04-12 15:44:17 +0200

    add missing include to util_macros

--- src/util_macros.h
+++ src/util_macros.h
@@ -1,6 +1,8 @@
 #ifndef UTIL_MACROS_H
 #define UTIL_MACROS_H
 
+#include "console_out.h"
+
 #define ASSERT( expression ) do{ if ( !(expression) ) { \
   error0("assertion \"%s\" failed (%s:%d)\n       bad choice of input parameters (please read the user manual in /doc).\n", \
   #expression, __FILE__, __LINE__ ); } }while(0)
