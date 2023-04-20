0a91f8e293854bdd67f020afa98b8e2a7eabbac6
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2023-04-20 14:04:21 +0200

    add explaining comments to test wrapper

--- test_wrapper.sh
+++ test_wrapper.sh
@@ -4,12 +4,14 @@ set -e
 
 [ -d testlogs ] || mkdir testlogs
 make clean 2>testlogs/cpu_clean.log >testlogs/cpu_clean.log
+
+# Build CPU version of program
 sed -i 's/CUDA_ENABLER =.*/CUDA_ENABLER =/g' MakeSettings.mk
 make -j 8 all 2>testlogs/cpu_build.log >testlogs/cpu_build.log
 ./test/integration_test.sh cpu
 
-[ -d testlogs ] || mkdir testlogs
 make clean 2>testlogs/gpu_clean.log >testlogs/gpu_clean.log
+# Build GPU version of program
 sed -i 's/CUDA_ENABLER =.*/CUDA_ENABLER = -DCUDA_OPT/g' MakeSettings.mk
 make -j 8 all 2>testlogs/gpu_build.log >testlogs/gpu_build.log
 ./test/integration_test.sh gpu
