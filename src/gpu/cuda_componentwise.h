3916e85e1eeb6430b7eba104ac9ff2d610e8bc61
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2023-07-06 14:03:03 +0000

    remove assembly prefetcher

--- src/gpu/cuda_componentwise.h
+++ src/gpu/cuda_componentwise.h
@@ -75,7 +75,6 @@ class ComponentAccess {
   }
 
   __device__ ElementType& operator[](size_t i) {
-    asm("prefetch.global.L1 [%0];" : : "l"(data + ((i + 1) * this->num_sites)));
     return data[i * this->num_sites];
   }
 
