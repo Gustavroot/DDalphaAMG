d3ee4d39d00d9fd4b3d5979fa3de69a35fc32186
kelong <klzhang@cnic.cn>
2024-02-01 10:53:46 +0800

    fixed setup on ORISE

--- setup_env.ORISE.sh
+++ setup_env.ORISE.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-source ./env.sh
+module purge
 
-echo "CPUS_INCLUDE_PATH = ${CPUS_INCLUDE_PATH}"
-echo "C_INCLUDE_PATH    = ${C_INCLUDE_PATH}   "
-echo "LD_LIBRARY_PATH   = ${LD_LIBRARY_PATH}  "
-echo "PATH   = ${PATH}  "
+module load compiler/devtoolset/7.3.1
+module load mpi/hpcx/2.11.0/gcc-7.3.1
+module load compiler/rocm/dtk/23.04
 
-echo "================================================"
-echo " "
-make -j 24
+# source /public/software/compiler/dtk/dtk-23.04/env.sh
+source /public/software/compiler/dtk/dtk-23.04/cuda/env.sh
+export C_INCLUDE_PATH=/public/software/compiler/dtk/dtk-23.04/cuda/include:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=/public/software/compiler/dtk/dtk-23.04/cuda/include:${CPLUS_INCLUDE_PATH}
+
+module load compiler/automake/1.15
+
