3c3a4ebd0da813090619a75db53b1f713c4f5a84
kelong <klzhang@cnic.cn>
2024-02-01 18:27:57 +0800

    Removed some developing files: 'benchmark/*'

--- benchmark/intrinsic_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm ./a.out -rf
-
-gcc -msse -mfma -mavx2 -O3 ./benchmark_simd_intrinsic.c
-
-./a.out
--- benchmark/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm ./a.out -rf
-
-g++ $1 $2 $3 -msse -mfma -mavx2 -O3 ./benchmark_vectorized_blas.cpp
-
-./a.out
